5c743a93e68e08bcce9cd031c256d0bdc7f3821c
Bina <benjaminbinyam500@gmail.com>
2025-02-14 10:26:21 +0300

    clear button functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,13 @@ const App = () => {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
+        <button
+          type="button"
+          onClick={() => setPeople([])}
+          className="btn btn-block"
+        >
+          clear all
+        </button>
       </section>
     </main>
   );
