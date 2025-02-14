da83713ccc1447f3176b85ca7456b7c0bf973d82
Bina <benjaminbinyam500@gmail.com>
2025-02-14 10:28:37 +0300

    add all button functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,23 @@ const App = () => {
       <section className="container">
         <h3>{people.length} birthdays today</h3>
         <List people={people} />
-        <button
-          type="button"
-          onClick={() => setPeople([])}
-          className="btn btn-block"
-        >
-          clear all
-        </button>
+        {people.length === 0 ? (
+          <button
+            type="button"
+            onClick={() => setPeople(data)}
+            className="btn btn-block"
+          >
+            Add all
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => setPeople([])}
+            className="btn btn-block"
+          >
+            clear all
+          </button>
+        )}
       </section>
     </main>
   );
